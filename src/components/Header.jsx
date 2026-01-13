3026133b75f9f6e14d11f13a2a9ec07fc89f6f6f
Jean Albertsen <jeanalbertsen@gmail.com>
2026-01-13 21:44:27 +0000

    Update website styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,11 +22,11 @@ export default function Header({ mobileOpen, setMobileOpen }) {
           {/* ✅ DESKTOP LOGO */}
           <span className="logoDesktop">
             <Logo
-              size={120}                 // ✅ BIGGER box on desktop
-              fontSize={80}
+              size={60}                 // ✅ BIGGER box on desktop
+              fontSize={24}
               borderWidth={4}
               borderColor="#0B387C"
-              nameSize={100}
+              nameSize={24}
               nameColor="rgba(255,255,255,0.72)"
               nameLetterSpacing="0.14em"
               nameMaxWidth={420}        // ✅ desktop can be wider
@@ -38,7 +38,7 @@ export default function Header({ mobileOpen, setMobileOpen }) {
           <span className="logoMobile">
             <Logo
               size={60}                 // ✅ smaller box on mobile
-              fontSize={25}
+              fontSize={20}
               borderWidth={3}
               borderColor="#0B387C"
               nameSize={20}
